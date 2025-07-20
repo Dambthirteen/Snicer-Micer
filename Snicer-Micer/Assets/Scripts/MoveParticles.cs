5a4ebbb159e3973f5bfa9204da34c267a9733287
Dambthirteen <danijasonlipke@gmail.com>
2025-07-20 14:40:43 +0200

    push kurz

--- Snicer-Micer/Assets/Scripts/MoveParticles.cs
+++ Snicer-Micer/Assets/Scripts/MoveParticles.cs
@@ -5,7 +5,6 @@ public class MoveParticles : MonoBehaviour
 
     [SerializeField] private ParticleSystem MoveParticleRight;
     private ParticleSystem _MoveparticleSystem;
-    [SerializeField] Transform RightTransform;
     [SerializeField] Transform LeftTransform;
     
     void Start()
@@ -16,14 +15,9 @@ public class MoveParticles : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
-            _MoveparticleSystem = Instantiate(MoveParticleRight, RightTransform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            _MoveparticleSystem = Instantiate(MoveParticleRight, LeftTransform.position, Quaternion.Euler(new Vector3(-90, 0, 0)));
+            _MoveparticleSystem = Instantiate(MoveParticleRight, LeftTransform.position, Quaternion.identity);
         }
     }
 }
